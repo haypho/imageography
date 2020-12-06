66d7fdd503250b2f94f5721d3da4aca838520952
hphothong <hphothong@gmail.com>
2020-12-06 12:37:35 -0700

    Add react-native-paper and icons

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
-/**
- * @format
- */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './src/App';
 import {name as appName} from './app.json';
+import {
+  Provider as PaperProvider,
+  DarkTheme,
+  DefaultTheme,
+} from 'react-native-paper';
+import {useColorScheme} from 'react-native';
 
-AppRegistry.registerComponent(appName, () => App);
+const Main = () => {
+  const colorScheme = useColorScheme();
+  const theme = colorScheme === 'dark' ? DarkTheme : DefaultTheme;
+
+  return (
+    <PaperProvider theme={theme}>
+      <App />
+    </PaperProvider>
+  );
+};
+
+AppRegistry.registerComponent(appName, () => Main);
