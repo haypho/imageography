56fdd0df3781f6a9971e34c8131b9df59090fa98
hphothong <hphothong@gmail.com>
2020-12-06 14:39:23 -0700

    Add prettier check to linter

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 import React from 'react';
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
 import {
   Provider as PaperProvider,
   DarkTheme,
   DefaultTheme,
 } from 'react-native-paper';
-import {useColorScheme} from 'react-native';
+import { useColorScheme } from 'react-native';
 
 const Main = () => {
   const colorScheme = useColorScheme();
