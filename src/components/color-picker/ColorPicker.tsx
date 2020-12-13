5f9b2cdb50601de3fc92c7e806be5ea08dcb2152
hphothong <hphothong@gmail.com>
2020-12-13 15:01:05 -0700

    Add smooth translation for color picker

--- src/components/color-picker/ColorPicker.tsx
+++ src/components/color-picker/ColorPicker.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import { IconButton } from 'react-native-paper';
 import { MARGIN } from '../../constants';
-import Animated from 'react-native-reanimated';
-import { PanGestureHandler } from 'react-native-gesture-handler';
+import { PanGestureHandler, State } from 'react-native-gesture-handler';
 import ColorSlider from '../color-slider';
+import Animated, { cond, eq, set, add, sub } from 'react-native-reanimated';
 
 const styles = StyleSheet.create({
   container: {
@@ -16,10 +16,60 @@ const styles = StyleSheet.create({
     flex: 1,
     borderRadius: 15,
   },
+  cursor: {
+    position: 'relative',
+  },
 });
 
 const ColorPicker: React.FC = () => {
   const [blendColor, setBlendColor] = useState<string>('#ff0000');
+
+  const state = useRef(new Animated.Value(-1)).current;
+
+  const dragX = useRef(new Animated.Value(0)).current;
+  const dragY = useRef(new Animated.Value(0)).current;
+
+  const transX = useRef(new Animated.Value(0)).current;
+  const transY = useRef(new Animated.Value(0)).current;
+
+  const prevDragX = useRef(new Animated.Value(0)).current;
+  const prevDragY = useRef(new Animated.Value(0)).current;
+
+  const onGestureEvent = useCallback(
+    Animated.event(
+      [
+        {
+          nativeEvent: {
+            translationX: dragX,
+            translationY: dragY,
+            state,
+          },
+        },
+      ],
+      { useNativeDriver: true },
+    ),
+    [],
+  );
+
+  const translateX = cond(
+    eq(state, State.ACTIVE),
+    [
+      set(transX, add(transX, sub(dragX, prevDragX))),
+      set(prevDragX, dragX),
+      transX,
+    ],
+    [set(dragX, 0), set(prevDragX, 0), transX],
+  );
+
+  const translateY = cond(
+    eq(state, State.ACTIVE),
+    [
+      set(transY, add(transY, sub(dragY, prevDragY))),
+      set(prevDragY, dragY),
+      transY,
+    ],
+    [set(dragY, 0), set(prevDragY, 0), transY],
+  );
 
   return (
     <View style={styles.container}>
@@ -29,9 +79,22 @@ const ColorPicker: React.FC = () => {
         locations={[0, 0.5, 1]}
         colors={['#fff', blendColor, '#000']}
         style={styles.linearGradient}>
-        <PanGestureHandler>
-          <Animated.View>
-            <IconButton icon="circle-outline" />
+        <PanGestureHandler
+          onGestureEvent={onGestureEvent}
+          onHandlerStateChange={onGestureEvent}>
+          <Animated.View
+            style={[
+              styles.cursor,
+              {
+                transform: [
+                  {
+                    translateX,
+                    translateY,
+                  },
+                ],
+              },
+            ]}>
+            <IconButton icon="circle-outline" color="white" size={30} />
           </Animated.View>
         </PanGestureHandler>
       </LinearGradient>
