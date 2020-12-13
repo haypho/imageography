e4060b98f2ee4822c63a79b6239ee64e4ee76a30
hphothong <hphothong@gmail.com>
2020-12-12 20:11:55 -0700

    Add sign out button on account screen

--- src/features/account/account.tsx
+++ src/features/account/account.tsx
@@ -1,13 +1,39 @@
-import React from 'react';
-import { Appbar, Text } from 'react-native-paper';
+import auth from '@react-native-firebase/auth';
+import React, { useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Appbar, Button } from 'react-native-paper';
+import { margin, padding } from '../../constants/styles';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: margin.medium,
+  },
+  content: {
+    flex: 1,
+  },
+  actionButtonContent: {
+    paddingVertical: padding.medium,
+  },
+});
 
 const Account: React.FC = () => {
+  const signOut = useCallback(() => auth().signOut(), []);
+
   return (
     <>
       <Appbar.Header>
         <Appbar.Content title="Account" />
       </Appbar.Header>
-      <Text>Account</Text>
+      <View style={styles.container}>
+        <View style={styles.content} />
+        <Button
+          icon="logout"
+          contentStyle={styles.actionButtonContent}
+          onPress={signOut}>
+          Sign Out
+        </Button>
+      </View>
     </>
   );
 };
