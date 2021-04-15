3c63cb0f68290946d48d348b516d0f639d14f988
Hayden Phothong <hphothong@gmail.com>
2021-04-14 18:23:24 -0600

    Add @app alias to project

--- babel.config.js
+++ babel.config.js
@@ -5,4 +5,16 @@ module.exports = {
       plugins: ['react-native-paper/babel'],
     },
   },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json'],
+        alias: {
+          '@app': './src/',
+        },
+      },
+    ],
+  ],
 };
