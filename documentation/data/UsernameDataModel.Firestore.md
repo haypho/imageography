5ea01e39b0e982ed9dd1ef7e090947c3ef831cc9
Hayden Phothong <hphothong@gmail.com>
2021-04-17 14:45:52 -0600

    #18 Remove users repository from firestore

--- documentation/data/UsernameDataModel.Firestore.md
+++ documentation/data/UsernameDataModel.Firestore.md
@@ -13,11 +13,15 @@ This data model is used for checking if usernames exist for unauthorized users.
 ## Definition
 
 ```
-{ }
+{
+  authorID: string
+}
 ```
 
 ## Example
 
 ```json
-{}
+{
+  "authorID": "iIgG62hFWQndW"
+}
 ```
--- src/services/repositories/username.repository.ts
+++ src/services/repositories/username.repository.ts
@@ -1,5 +1,7 @@
 import firestore from '@react-native-firebase/firestore';
 import { FirestoreCollection } from '@app/constants';
+import { FirebaseAuthTypes } from '@react-native-firebase/auth';
+import auth from '@react-native-firebase/auth';
 
 export class UsernameRepository {
   public static async usernameExists(username: string): Promise<boolean> {
@@ -16,10 +18,15 @@ export class UsernameRepository {
 
   public static async addUsername(username: string): Promise<boolean> {
     try {
+      const user: FirebaseAuthTypes.User | null = auth().currentUser;
+      if (!user) {
+        return false;
+      }
+
       await firestore()
         .collection(FirestoreCollection.Usernames)
         .doc(username.toLowerCase())
-        .set({});
+        .set({ authorID: user.uid });
       return true;
     } catch (e) {
       console.error(e);
