b7ab86de48038f4c704916a0c6b10f9637b0681f
Hayden Phothong <hphothong@gmail.com>
2021-04-25 14:27:53 -0600

    #12 Clamp cursor correctly to gradient

--- src/components/color-picker/ColorPicker.tsx
+++ src/components/color-picker/ColorPicker.tsx
@@ -1,11 +1,87 @@
-import React, { useCallback, useRef, useState } from 'react';
-import { StyleSheet, View } from 'react-native';
+import React, { useCallback, useState } from 'react';
+import { LayoutChangeEvent, StyleSheet, View } from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
-import { IconButton } from 'react-native-paper';
-import { MARGIN } from '../../constants';
-import { PanGestureHandler, State } from 'react-native-gesture-handler';
-import ColorSlider from '../color-slider';
-import Animated, { cond, eq, set, add, sub } from 'react-native-reanimated';
+import { PanGestureHandler, PanGestureHandlerGestureEvent } from 'react-native-gesture-handler';
+import Animated, {
+  useSharedValue,
+  useAnimatedStyle,
+  useAnimatedGestureHandler,
+  interpolate,
+  Extrapolate,
+} from 'react-native-reanimated';
+import { MARGIN } from '@app/constants';
+import ColorSlider from '@app/components/color-slider';
+
+type AnimatedContext = {
+  startX: number;
+  startY: number;
+};
+
+const CURSOR_SIZE: number = 32;
+
+const ColorPicker: React.FC = () => {
+  const [gradientLayout, setGradientLayout] = useState<{ x: number; y: number }>({ x: 300, y: 300 });
+  const [blendColor, setBlendColor] = useState<string>('#ff0000');
+
+  const setBounds = useCallback(
+    (event: LayoutChangeEvent) => {
+      const { width, height } = event.nativeEvent.layout;
+      setGradientLayout({ x: width - CURSOR_SIZE, y: height - CURSOR_SIZE });
+    },
+    [setGradientLayout],
+  );
+
+  const translation = {
+    x: useSharedValue(0),
+    y: useSharedValue(0),
+  };
+
+  const gestureHandler = useAnimatedGestureHandler<PanGestureHandlerGestureEvent, AnimatedContext>(
+    {
+      onStart: (_, context) => {
+        context.startX = translation.x.value;
+        context.startY = translation.y.value;
+      },
+      onActive: (event, context) => {
+        translation.x.value = context.startX + event.translationX;
+        translation.y.value = context.startY + event.translationY;
+      },
+      onEnd: () => {
+        console.log('x', translation.x.value, 'y', translation.y.value);
+      },
+    },
+    [],
+  );
+  const animatedStyles = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: interpolate(translation.x.value, [0, gradientLayout.x], [0, gradientLayout.x], Extrapolate.CLAMP),
+        },
+        {
+          translateY: interpolate(translation.y.value, [0, gradientLayout.y], [0, gradientLayout.y], Extrapolate.CLAMP),
+        },
+      ],
+    };
+  });
+
+  return (
+    <View style={styles.container}>
+      <LinearGradient
+        start={{ x: 0, y: 0 }}
+        end={{ x: 1, y: 1 }}
+        locations={[0, 0.5, 1]}
+        colors={['#fff', blendColor, '#000']}
+        style={styles.linearGradient}
+        onLayout={setBounds}>
+        <PanGestureHandler onGestureEvent={gestureHandler}>
+          <Animated.View style={[styles.cursor, animatedStyles]} />
+        </PanGestureHandler>
+      </LinearGradient>
+      <ColorSlider onValueChange={setBlendColor} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
@@ -16,113 +92,12 @@ const styles = StyleSheet.create({
     flex: 1,
     borderRadius: 15,
   },
-  cursor: {},
+  cursor: {
+    height: CURSOR_SIZE,
+    width: CURSOR_SIZE,
+    borderWidth: 3,
+    borderRadius: Math.floor(CURSOR_SIZE / 2),
+  },
 });
-
-const ColorPicker: React.FC = () => {
-  const [blendColor, setBlendColor] = useState<string>('#ff0000');
-
-  const state = useRef(new Animated.Value(-1)).current;
-
-  const dragX = useRef(new Animated.Value(0)).current;
-  const dragY = useRef(new Animated.Value(0)).current;
-
-  const transX = useRef(new Animated.Value(0)).current;
-  const transY = useRef(new Animated.Value(0)).current;
-
-  const prevDragX = useRef(new Animated.Value(0)).current;
-  const prevDragY = useRef(new Animated.Value(0)).current;
-
-  const onGestureEvent = useCallback(
-    Animated.event(
-      [
-        {
-          nativeEvent: {
-            translationX: dragX,
-            translationY: dragY,
-            state,
-          },
-        },
-      ],
-      { useNativeDriver: true },
-    ),
-    [],
-  );
-
-  const translateX = cond(
-    eq(state, State.ACTIVE),
-    [
-      set(transX, add(transX, sub(dragX, prevDragX))),
-      set(prevDragX, dragX),
-      transX.interpolate({
-        inputRange: [-15, 345],
-        outputRange: [-15, 345],
-        extrapolate: Animated.Extrapolate.CLAMP,
-      }),
-    ],
-    [
-      set(dragX, 0),
-      set(prevDragX, 0),
-      transX.interpolate({
-        inputRange: [-15, 345],
-        outputRange: [-15, 345],
-        extrapolate: Animated.Extrapolate.CLAMP,
-      }),
-    ],
-  );
-
-  const translateY = cond(
-    eq(state, State.ACTIVE),
-    [
-      set(transY, add(transY, sub(dragY, prevDragY))),
-      set(prevDragY, dragY),
-      transY.interpolate({
-        inputRange: [-16, 187],
-        outputRange: [-16, 187],
-        extrapolate: Animated.Extrapolate.CLAMP,
-      }),
-    ],
-    [
-      set(dragY, 0),
-      set(prevDragY, 0),
-      transY.interpolate({
-        inputRange: [-16, 187],
-        outputRange: [-16, 187],
-        extrapolate: Animated.Extrapolate.CLAMP,
-      }),
-    ],
-  );
-
-  return (
-    <View style={styles.container}>
-      <LinearGradient
-        start={{ x: 0, y: 0 }}
-        end={{ x: 1, y: 1 }}
-        locations={[0, 0.5, 1]}
-        colors={['#fff', blendColor, '#000']}
-        style={styles.linearGradient}>
-        <PanGestureHandler
-          onGestureEvent={onGestureEvent}
-          onHandlerStateChange={onGestureEvent}>
-          <Animated.View
-            style={[
-              styles.cursor,
-              {
-                transform: [
-                  {
-                    translateX,
-                    translateY,
-                  },
-                ],
-              },
-            ]}>
-            <IconButton icon="circle-outline" color="grey" size={30} />
-          </Animated.View>
-        </PanGestureHandler>
-      </LinearGradient>
-      <ColorSlider onValueChange={setBlendColor} />
-    </View>
-  );
-};
 
 export default ColorPicker;
